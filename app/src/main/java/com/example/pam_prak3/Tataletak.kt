797f61ee83fff20b4d3ce185f750da77b4c22052
Othallia <vianiputriocta6@gmail.com>
2025-10-17 15:28:00 +0700

    buat box sederhana

--- app/src/main/java/com/example/pam_prak3/Tataletak.kt
+++ app/src/main/java/com/example/pam_prak3/Tataletak.kt
@@ -45,3 +45,18 @@ fun TataletakRow(modifier: Modifier) {
         Text(text = "Komponen 4")
     }
 }
+
+@Composable
+fun TataletakBox(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Box 1")
+        Text(text = "Column 1")
+        Text(text = "Row 1")
+        Text(text = "Row 2")
+    }
+}
