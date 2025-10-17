63716ea6ae358ef867d99b9bf0ce7e5129cf0959
Othallia <vianiputriocta6@gmail.com>
2025-10-17 15:36:49 +0700

    Composable TataletakBoxColumnRow dengan Box kuning

--- app/src/main/java/com/example/pam_prak3/Tataletak.kt
+++ app/src/main/java/com/example/pam_prak3/Tataletak.kt
@@ -103,3 +103,14 @@ fun TataletakRowColumn(modifier: Modifier) {
         }
     }
 }
+
+@Composable
+fun TataletakBoxColumnRow(modifier: Modifier) {
+    val gambar = painterResource(id = R.drawable.ic_launcher_background) // Pastikan nama drawable benar
+    Column {
+        Box(
+            modifier = modifier
+                .height(110.dp)
+                .background(color = Color.Yellow),
+            contentAlignment = Alignment.Center
+        )
