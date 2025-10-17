3d86f06e92c3c155bd2fd5e44e01485624694bac
Othallia <vianiputriocta6@gmail.com>
2025-10-17 15:56:57 +0700

    ubah nama gambar

--- app/src/main/java/com/example/pam_prak3/Tataletak.kt
+++ app/src/main/java/com/example/pam_prak3/Tataletak.kt
@@ -107,7 +107,7 @@ fun TataletakRowColumn(modifier: Modifier) {
 
 @Composable
 fun TataletakBoxColumnRow(modifier: Modifier) {
-    val gambar = painterResource(id = R.drawable.ic_launcher_background) // Pastikan nama drawable benar
+    val gambar = painterResource(id = R.drawable.gambar)
     Column {
         Box(
             modifier = modifier
