408693224fca9821dcf6c5f7bd1cb234b8b69a4b
Othallia <vianiputriocta6@gmail.com>
2025-10-17 16:08:59 +0700

    ganti gambar

--- app/src/main/java/com/example/pam_prak3/Tataletak.kt
+++ app/src/main/java/com/example/pam_prak3/Tataletak.kt
@@ -107,7 +107,7 @@ fun TataletakRowColumn(modifier: Modifier) {
 
 @Composable
 fun TataletakBoxColumnRow(modifier: Modifier) {
-    val gambar = painterResource(id = R.drawable.gambar)
+    val gambar = painterResource(id = R.drawable.avatar)
     Column {
         Box(
             modifier = modifier
